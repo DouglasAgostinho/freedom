c0f3530f38689486431ae3fa10e8511928c8ba1e
DouglasAgostinho <marques.aut@gmail.com>
2024-06-11 11:47:42 -0300

    Change user input to a side thread and implement message passing through channel

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,47 @@
 mod net;
 mod block;
 
+use core::time;
 use std::io; 
 use std::thread;
 use net::network;
+use std::sync::mpsc::{self, Sender};
+//use std::sync::mpsc::{self, Receiver, Sender};
 use block::{Block, Node};
+
 
 //Constant to use in String based variables
 const EMPTY_STRING: String = String::new();
 
+fn handle_input(node: &Node, tx: Sender<[String; 3]>){
+
+    loop {                
+        //Variable to receive user input
+        let mut user_input = EMPTY_STRING;
+
+        //Get user input
+        println!("Please enter the message");
+        match io::stdin().read_line(&mut user_input) {
+            Ok(_) => (),
+            Err(e) => println!("Error found {}", e),
+        }        
+
+        //Organize data to fit in the message format [current time, address, message text]
+        let message: [String; 3] = [node.get_time_ns(), node.address.clone(), String::from(user_input.trim())];
+
+        if tx.send(message).is_err() {
+            eprintln!("Failed to send input to main thread.");
+            break;
+        }
+    }
+    
+}
 
 fn main() {
     //Initial greetins
     println!("Welcome to FREDOOM !!!");
+
+    let (send_to_main, main_receiver) = mpsc::channel();
 
     //Spawn thread for server initialization    
     thread::spawn( || network::net_init());
@@ -27,25 +56,34 @@ fn main() {
     let mut my_node: Node = Node{address:EMPTY_STRING};
     my_node.address = my_node.gen_address();
 
+
+    thread::spawn(move || {handle_input(&my_node, send_to_main)});
+
     loop{
 
-        //Variable to receive user input
-        let mut user_input = EMPTY_STRING;
-
-        //Get user input
-        println!("Please enter the message");
-        match io::stdin().read_line(&mut user_input) {
-            Ok(_) => (),
-            Err(e) => println!("Error found {}", e),
-        }        
-
-        //Organize data to fit in the message format [current time, address, message text]
-        let message: [String; 3] = [my_node.get_time_ns(), my_node.address.clone(), String::from(user_input.trim())];       
+        // Check for new messages from the input thread
+        let message = match main_receiver.try_recv() {
+            Ok(input) => {
+                println!("Received input: {:?}", input);
+                input
+            },
+            Err(mpsc::TryRecvError::Empty) => {
+                // No input received, continue with other work
+                continue;
+            }
+            Err(mpsc::TryRecvError::Disconnected) => {
+                eprintln!("Input thread has disconnected.");
+                break;
+            }
+        };
+               
 
         //Call insert function to format and store in a block section
         blocks.insert(message.clone());
 
         println!("{:?}", blocks.message );
+
+        thread::sleep(time::Duration::from_millis(2000));
     }
 
 }
