8d51a51f75faa701c7aff2ecbf87396a177221a5
DouglasAgostinho <marques.aut@gmail.com>
2024-06-11 04:52:01 -0300

    code reorganization & change client/server operation

--- src/main.rs
+++ src/main.rs
@@ -1,138 +1,49 @@
-
-
+//Modules declaration
 mod net;
 mod block;
 
-
 use std::io; 
-
-//use net::network;
+use net::network;
 use block::{Block, Node};
-//use serde::{Deserialize, Serialize};
 
-
+//Constant to use in String based variables
 const EMPTY_STRING: String = String::new();
-
 
 // This is the main function
 fn main() {
-
+    //Initial greetins
     println!("Welcome to FREDOOM !!!");
 
-    //network::net_init();
+    //Server initialization
+    network::net_init();
 
-    //let mut message: [String; 3] = [EMPTY_STRING; 3];
-    //let index: [&str; 3] = ["Time", "Addr", "message"];
-
-    let mut item = String::new();
-
-    let mut my_block = Block{
+    //Instance of Block struct
+    let mut blocks: Block = Block{
         message: Vec::from([[EMPTY_STRING; 3]])
     };
 
-    let mut my_node = Node{address:EMPTY_STRING};
+    //Instance of Node struct
+    let mut my_node: Node = Node{address:EMPTY_STRING};
     my_node.address = my_node.gen_address();
-
-    //let address = String::from("192.168.191.1:8687");
-
-    loop{        
-
-        println!("Please enter the message");
-
-        match io::stdin().read_line(&mut item) {
-            Ok(_) => (),
-            Err(e) => println!("Error found {}", e),
-            
-        }        
-
-        let message = [my_node.get_time_ns(), my_node.address.clone(), String::from(item.trim())];
-
-        item = String::from("");        
-    
-        println!("Print Vector {:?}", message);
-
-        println!("Print Vector {:?}", my_block.message);
-
-        //my_block.message.push(message.clone());
-
-        my_block.insert(message.clone());
-
-        println!("Print Vector {:?}", my_block.message);
-
-        println!("Pre Serde {:?}", my_block);
-        let v = serde_json::to_value(&my_block).unwrap();
-        
-        let document: Block = serde_json::from_value(v).unwrap();
-        println!("Pos Serde {:?}", document);
-    }
-
-}
-
-
-/*
-mod net;
-mod block;
-
-use std::io;
-//use net::network;
-use block::Block;
-
-const EMPTY_STRING: String = String::new();
-
-
-fn main() {
-    println!("Welcome to FREDOOM !!!");
-
-    //network::net_init();
-
-    let mut message: [String; 3] = [EMPTY_STRING; 3];
-    let index: [&str; 3] = ["Time", "Addr", "message"];
-
-    let mut item = String::new();
-
-    let mut my_block = Block{
-        message: Vec::from([[EMPTY_STRING; 3]])
-    };
-
-    //let address = String::from("192.168.191.1:8687");
 
     loop{
 
-        for i in 0..3{
+        //Variable to receive user input
+        let mut user_input = EMPTY_STRING;
 
-            println!("Please enter {}", index[i]);
-    
-            match io::stdin().read_line(&mut item) {
-                Ok(_) => (),
-                Err(e) => println!("Error found {}", e),
-                
-            }        
-    
-            message[i] = String::from(item.trim());
-    
-            item = String::from("");
-        }
-    
-        println!("Print Vector {:?}", message);
+        //Get user input
+        println!("Please enter the message");
+        match io::stdin().read_line(&mut user_input) {
+            Ok(_) => (),
+            Err(e) => println!("Error found {}", e),
+        }        
 
-        println!("Print Vector {:?}", my_block.message);
+        //Organize data to fit in the message format [current time, address, message text]
+        let message: [String; 3] = [my_node.get_time_ns(), my_node.address.clone(), String::from(user_input.trim())];       
 
-        //my_block.message.push(message.clone());
-
-        my_block.add(message.clone());
-
-        println!("Print Vector {:?}", my_block.message);
+        //Call insert function to format and store in a block section
+        blocks.insert(message.clone());
 
     }
 
-*/    
-    
-
-    /*println!("{}", message);
-
-    match network::client(&message, &address){
-        Ok(_) => (),
-        Err(e) => println!("Error found {}", e),
-    }
-    */
-
+}
--- src/net.rs
+++ src/net.rs
@@ -2,31 +2,104 @@ pub mod network{
 
     use std::net::{TcpListener, TcpStream};
     use std::thread;
-    use std::io::{self,Read, Write, Error};
+    //use std::io::{self,Read, Write, Error};
+    use std::io::{self,Read, Write};
 
-    fn handle_client(mut stream: TcpStream) -> Result<(), Error>{
 
-        println!("Incoming connection from {}", stream.peer_addr()?);        
+    fn handle_message(message: &String, mode: &str) -> u8{
+        //Function to treat incoming / outgoing messages
+
+        match mode {
+
+            "encrypt" => {0},
+
+            "decrypt_llm" => {0},
+
+            "interpret" => {
+                match message {
+
+                    
+
+                    _ => 0,
+                }
+            },
+
+            "test" => {
+                println!("Received: {}", message);
+                254
+            },
+            _ => 0,
+        }
+    }
+
+
+    fn handle_client(mut stream: TcpStream, mode: &str) {
+        //fn handle_client(mut stream: TcpStream) -> Result<(), Error>{
+
+        //println!("Incoming connection from {}", stream.peer_addr()?);   
+        println!("Incoming connection from {}", stream.peer_addr().expect("Error"));
         let mut buf = [0; 512];
 
         loop {
             
-            let bytes_read = stream.read(&mut buf)?;
-            if bytes_read == 0 {return Ok(())}
-            stream.write(&buf[..bytes_read])?;
+            //let bytes_read = stream.read(&mut buf)?;
+            let bytes_read = stream.read(&mut buf).expect("Error");
+            //if bytes_read == 0 {return Ok(())}
+            if bytes_read == 0 {break}
+            let received = String::from_utf8_lossy(&buf[..bytes_read]);
+            match handle_message(&received.to_string(), mode) {
+
+                1 => {
+                    //Receive blocks from server
+                    let mut buffer = [0; 1024];
+                    //let bytes_read = stream.read(&mut buffer)?;
+                    let bytes_read = stream.read(&mut buffer).expect("Error");
+                    let received = String::from_utf8_lossy(&buffer[..bytes_read]);
+                    //println!("Received: {}", received);
+                },
+                2 => {
+                    // Receive data continuously from the server
+                    let mut buffer = [0; 1024];                    
+                    loop {
+                        match stream.read(&mut buffer) {
+                            Ok(0) => {
+                                println!("Connection closed by server");
+                                break;
+                            },
+                            Ok(n) => {
+                                let msg = String::from_utf8_lossy(&buffer[0..n]);
+                                print!("{}", msg);      //Uses print! to not insert /n after each received data                                
+                                //io::stdout().flush()?;  // Ensure immediate output
+                                io::stdout().flush().expect("error");  // Ensure immediate output
+                            },
+                            Err(e) => {
+                                println!("Failed to receive message: {}", e);
+                                break;
+                            }
+                        }
+                    }
+                    //Ok(())
+                },
+
+                _ => {},
+            }
+            
         }
     }
+
 
     pub fn net_init(){
 
         let listener = TcpListener::bind("0.0.0.0:6886").expect("Could not bind");
+        println!("Server initialized...");
 
         for stream in listener.incoming(){
             match stream {
                 Err(e) => println!("Error found {e}"),
                 Ok(stream) => {
                     thread::spawn(move || {
-                        handle_client(stream).unwrap_or_else(|error| println!("Error {:?}", error));
+                        //handle_client(stream).unwrap_or_else(|error| println!("Error {:?}", error));
+                        handle_client(stream, "interpret");
                     });
                 }
             }
@@ -35,92 +108,31 @@ pub mod network{
 
 
     pub fn client(message: &str, address: &str, mode: i32)-> io::Result<()> {
-        
-
         match mode {
-
             1 => {
                 // Connect to the server
                 let mut stream = TcpStream::connect(address)?;
-
                 // Send data to the server
                 stream.write_all(message.as_bytes())?;
-
-                // Receive data from the server
-                let mut buffer = [0; 1024];
-                
-                loop {
-                    match stream.read(&mut buffer) {
-                        Ok(0) => {
-                            println!("Connection closed by server");
-                            break;
-                        },
-                        Ok(n) => {
-
-                            let msg = String::from_utf8_lossy(&buffer[0..n]);
-                            print!("{}", msg);
-                            
-                            //io::stdout().flush()?;  // Ensure immediate output
-                            
-                        },
-                        Err(e) => {
-                            println!("Failed to receive message: {}", e);
-                            break;
-                        }
-                    }
-                }
                 Ok(())
-
             },
-
             2 => {
-
                 // Connect to the server
                 let mut stream = TcpStream::connect(address)?;
 
-                // Send data to the server
-                //let message = "Hello, server!";
-                stream.write_all(message.as_bytes())?;
+                //println!("Pre Serde {:?}", message);
 
-                // Receive data from the server
-                let mut buffer = [0; 1024];
-                let bytes_read = stream.read(&mut buffer)?;
-                let received = String::from_utf8_lossy(&buffer[..bytes_read]);
-                println!("Received: {}", received);
-
+                let serialized = serde_json::to_string(&message)?;
+                stream.write_all(serialized.as_bytes())?;
                 Ok(())
-
             },
-
+            3 => {
+                // Connect to the server
+                let mut stream = TcpStream::connect(address)?;
+                stream.write_all(message.as_bytes())?;
+                Ok(())
+            },
             _ => Ok(()),
-            
         }
-        
     }
-    
 }
-
-
-/*
-
-pub fn client(message: &str, address: &str)-> io::Result<()> {
-        //use std::net::TcpStream;
-        //use std::io::{self, Write, Read};
-
-        // Connect to the server
-        let mut stream = TcpStream::connect(address)?;
-
-        // Send data to the server
-        //let message = "Hello, server!";
-        stream.write_all(message.as_bytes())?;
-
-        // Receive data from the server
-        let mut buffer = [0; 1024];
-        let bytes_read = stream.read(&mut buffer)?;
-        let received = String::from_utf8_lossy(&buffer[..bytes_read]);
-        println!("Received: {}", received);
-
-        Ok(())
-    }    
-
-*/
