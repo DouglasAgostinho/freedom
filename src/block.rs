8e9db35cbd90db762d316ebe03cc7513e14eefc4
DouglasAgostinho <marques.aut@gmail.com>
2024-06-25 00:52:06 -0300

    Dead code removal, code documentation

--- src/block.rs
+++ src/block.rs
@@ -1,20 +1,19 @@
 
 use sha2::{Digest, Sha512};
 use std::time::{SystemTime,UNIX_EPOCH};
-//use serde::{Deserialize, Serialize};
 
-//#[derive(Debug, Deserialize, Serialize)]
-//#[derive(Debug, serde::Deserialize, serde::Serialize)]
 pub struct Block {        
     pub message:    Vec<[String; 3]>,    
 }
 impl Block {
 
-    //pub fn add (&mut self, time: String, addr: String, msg: String) {
+    ///Insert function works to keep the blocks updated and organized
     pub fn insert (&mut self,msg: [String; 3]) {
 
+        //Insert data in the Vector
         self.message.push(msg);
 
+        //Organize data based on creation time (index0)
         self.message.sort_by(|a, b| a[0].cmp(&b[0]));
     }       
 }
@@ -25,6 +24,7 @@ pub struct Node {
 }
 impl Node {
 
+    ///Node address generation
     pub fn gen_address(&self) -> String{
 
         let mut hasher = Sha512::new();
@@ -35,22 +35,11 @@ impl Node {
         // Read hash digest and consume hasher
         let result = hasher.finalize();
     
-        //let hs = String::from("{result}");
-
-
         // Convert the byte array to a hexadecimal string
-        //let hash_string: String = result.iter().map(|byte| format!("{:02x}", byte)).collect();
         result.iter().map(|byte| format!("{:02x}", byte)).collect()
-
-
-        //String::from("{hasher.finalize()}")
-        
-
-        // Print the hash as a hexadecimal string
-        //println!("Hash: {:x}", result);
-
     }
 
+    ///Function responsible to get and return current time in nanoseconds
     pub fn get_time_ns(&self) -> String{
 
         let start = SystemTime::now();
@@ -61,26 +50,3 @@ impl Node {
     }
     
 }
-
-
-/* 
-pub struct Letter {        
-    pub message:    Vec<[String; 3]>,
-    
-}
-impl Letter {
-
-    //pub fn add (&mut self, time: String, addr: String, msg: String) {
-    pub fn add (&mut self,msg: [String; 3]) {
-
-        self.message.push(msg);
-
-        self.message.sort_by(|a, b| a[0].cmp(&b[0]));
-    }
-    
-}
-
-pub mod block{
-
-    
-}*/
